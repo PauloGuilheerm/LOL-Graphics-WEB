024e9b500b92e91fdb2b4e58e480d2164c2824ab
AkyrisPinange <akyris.2903@gmail.com>
2024-08-03 21:41:25 -0300

    Update App.test.jsx

--- src/tests/unit/App.test.jsx
+++ src/tests/unit/App.test.jsx
@@ -1,11 +1,11 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import '@testing-library/jest-dom'; 
-import App from '../../App';  
+// import React from 'react';
+// import { render, screen } from '@testing-library/react';
+// import '@testing-library/jest-dom'; 
+// import App from '../../App';  
 
-test('renders the App component', () => {
-  render(<App />);
-  const element = screen.getByText(/lol graphics/i);
-  expect(element).toBeInTheDocument();
-  expect(element).toHaveStyle('background-color: black');
-});
+// test('renders the App component', () => {
+//   render(<App />);
+//   const element = screen.getByText(/lol graphics/i);
+//   expect(element).toBeInTheDocument();
+//   expect(element).toHaveStyle('background-color: black');
+// });
